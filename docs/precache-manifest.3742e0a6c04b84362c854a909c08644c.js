eae47dcbf43af062fb2dd688f67d880c14c0a98b
Anatoly Popov <a.popov@zarplata.ru>
2019-07-08 22:45:30 +0700

    troll pick

---
+++ docs/precache-manifest.3742e0a6c04b84362c854a909c08644c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eb8c932b68b6a293911973a32d57ce4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d7a059c1cf4fe52c1b3",
+    "url": "/static/css/main.e00633b6.chunk.css"
+  },
+  {
+    "revision": "17cbd3cc90cebf4e9ddd",
+    "url": "/static/js/2.0254dc8a.chunk.js"
+  },
+  {
+    "revision": "3d7a059c1cf4fe52c1b3",
+    "url": "/static/js/main.551428a0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,134 @@
 import React from 'react';
-import logo from './logo.svg';
+import cx from 'classnames';
+import shuffle from 'lodash/shuffle';
+import chunk from 'lodash/chunk';
+import flatten from 'lodash/flatten';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const savedLength = Number(localStorage.getItem('savedLength')) || 2;
+const savedNumbers = localStorage.getItem('savedNumbers') ? JSON.parse(localStorage.getItem('savedNumbers')) : {};
+
+const createArray = (length) => {
+    return Array(length).fill(length).map((_, i) => i);
+};
+
+class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            array: this.shuffle(savedLength, savedNumbers),
+            length: savedLength,
+            savedNumbers: savedNumbers
+        };
+    }
+
+    timer;
+
+    componentDidMount() {
+        this.startTimer(this.state.length)
+    }
+
+    startTimer(length) {
+        const time = 800 - (length * 10);
+
+        this.timer = setInterval(() => {
+            this.setState({
+                array: this.shuffle(this.state.length, this.state.savedNumbers)
+            })
+        }, time < 350 ? 350 : time);
+    }
+
+    handleToggle = () => {
+        this.checkArray();
+    }
+
+    shuffle = (length, savedNumbers) => {
+        const array = createArray(length);
+        let shuffleNumbers = array.filter((item) => {
+            return !savedNumbers[item];
+        });
+
+        shuffleNumbers = flatten(chunk(shuffleNumbers, 6).map(a => shuffle(a)));
+
+        const data = array.map(item => savedNumbers[item] ? item : shuffleNumbers.shift());
+
+        return data;
+    }
+
+    checkArray = () => {
+        const copyArray = createArray(this.state.length);
+        let savedNumbers = {};
+        const resetLength = Object.keys(this.state.savedNumbers).length;
+
+        const isGood = this.state.array.every((item, i) => {
+            const isGood = item === copyArray[i];
+
+            if (isGood) {
+                savedNumbers[item] = true;
+
+                return isGood
+            }
+
+            if (resetLength === i) {
+                savedNumbers = {};
+            }
+
+            return isGood;
+        });
+
+        if (!isGood) {
+            this.setState({
+                isRun: false,
+                savedNumbers
+            });
+
+            localStorage.setItem('savedNumbers', JSON.stringify(savedNumbers));
+
+            return;
+        }
+
+        const newLength = this.state.length + 1;
+
+        localStorage.setItem('savedLength', newLength);
+        localStorage.setItem('savedNumbers', JSON.stringify({}));
+
+        clearInterval(this.timer);
+
+        this.setState({
+            array: this.shuffle(newLength, {}),
+            length: newLength,
+            savedNumbers: {}
+        });
+
+        this.startTimer(newLength)
+    }
+
+    renderItem = (i) => <div key={i} className={cx('item', { active: this.state.savedNumbers[i] })}>{i}</div>;
+
+    render() {
+        const { array } = this.state;
+
+        return (
+            <div className="wrapper">
+                <div className="title">
+                    Easy sort array
+                </div>
+                <div className="sub-title">
+                    level:<div className="points"> {this.state.length - 2}</div>
+                </div>
+
+                <div className="array">
+                    {array.map(this.renderItem)}
+                </div>
+
+                <div className="button" onClick={this.handleToggle}>
+                    Check
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
